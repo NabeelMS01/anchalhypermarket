dfaec37e2ed76c993724927cdcf88911841875b5
NabeelMS01 <nabeelmampallil123@gmail.com>
2025-04-05 11:59:06 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Roboto_Flex } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head />
       <body className={googleFont.className}>
         <Providers>
           <Navbar />
